7178c35ae3f3a655e7964932de7b1c0e32536c98
joel <joel.gonzalez@iesjoandaustria.org>
2017-04-04 19:56:55 +0200

    commit 6

--- app/src/main/java/com/example/epickpc/epickbets2/ActionsActivity.java
+++ app/src/main/java/com/example/epickpc/epickbets2/ActionsActivity.java
@@ -1,13 +1,22 @@
 package com.example.epickpc.epickbets2;
 
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class ActionsActivity extends AppCompatActivity{
+    TextView texto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actions);
-
+    texto = (TextView) findViewById(R.id.texto);
     }
+
+    public void Cambia_texto(View v){
+        texto.setText("Username= ");
+    }
+
 }
