359997fe88b5708449410604d6015f0b715908be
jowmklow <joel.gonzalez@iesjoandaustria.org>
2017-04-05 14:02:36 +0200

    commit login comment1

--- app/src/main/java/com/example/epickpc/epickbets2/ActionsActivity.java
+++ app/src/main/java/com/example/epickpc/epickbets2/ActionsActivity.java
@@ -2,31 +2,31 @@ package com.example.epickpc.epickbets2;
 
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.TextView;
 
 
 public class ActionsActivity extends AppCompatActivity{
     TextView tnombre;
     TextView tmovil;
-    Boolean pass;
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actions);
 
-    tnombre = (TextView) findViewById(R.id.tnombre);
-        tnombre.setText(getIntent().getExtras().getString("username"));
-    tmovil = (TextView) findViewById(R.id.tmovil);
+        tnombre = (TextView) findViewById(R.id.tnombre);
+        tnombre.setText("Hola: " + getIntent().getExtras().getString("username"));
+
+        tmovil = (TextView) findViewById(R.id.tmovil);
+        tmovil.setText("Password: " + getIntent().getExtras().getString("password"));
 
 
+        String id = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        //displaying id in textview
 
-    }
-    public void cambiaTexto(View v){
-
-        tnombre.setText("Username= ");
-        tmovil.setText("Device ID= ");
+        tv = (TextView) findViewById(R.id.tdeviceId);
+        tv.setText("Token ID: " + id);
     }
 }
--- app/src/main/java/com/example/epickpc/epickbets2/MainActivity.java
+++ app/src/main/java/com/example/epickpc/epickbets2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.epickpc.epickbets2;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -9,17 +10,20 @@ import android.provider.Settings;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity implements LoginListener {
-
+    /*private CheckBox saveLoginCheckBox;
+    private SharedPreferences loginPreferences;
+    private SharedPreferences.Editor loginPrefsEditor;
+    private Boolean saveLogin;*/
     @BindView(R.id.entra_nom_inici)
     EditText entraNom;
     @BindView(R.id.entra_contrasenya_inici)
@@ -30,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements LoginListener {
     ImageView imagenLogo;
     Activity c;
     MyAsyncTask m;
-    TextView texto;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,15 @@ public class MainActivity extends AppCompatActivity implements LoginListener {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         c = this;
+        /*saveLoginCheckBox = (CheckBox)findViewById(R.id.saveLoginCheckBox);
+        loginPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
+        loginPrefsEditor = loginPreferences.edit();
+        saveLogin = loginPreferences.getBoolean("saveLogin", false);
+        if (saveLogin == true) {
+            entraNom.setText(loginPreferences.getString("username", ""));
+            entraContra.setText(loginPreferences.getString("password", ""));
+            saveLoginCheckBox.setChecked(true);
+        }*/
 
         ConnectivityManager cManager = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
         NetworkInfo nInfo = cManager.getActiveNetworkInfo();
@@ -56,6 +69,23 @@ public class MainActivity extends AppCompatActivity implements LoginListener {
         Intent i;
         switch (view.getId()) {
             case R.id.inicia_sessio:
+                if (true) {
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(entraNom.getWindowToken(), 0);
+
+                    /*if (saveLoginCheckBox.isChecked()) {
+                        loginPrefsEditor.putBoolean("saveLogin", true);
+                        loginPrefsEditor.putString("username", entraNom.getText().toString());
+                        loginPrefsEditor.putString("password", entraContra.getText().toString());
+                        loginPrefsEditor.commit();
+                    } else {
+                        loginPrefsEditor.clear();
+                        loginPrefsEditor.commit();
+                    }
+                    doSomethingElse();*/
+                }
+
+
                 String deviceId = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
                 m = new MyAsyncTask(entraNom.getText().toString(),entraContra.getText().toString(),deviceId,this);
                         /*Thread.start();
@@ -82,10 +112,15 @@ public class MainActivity extends AppCompatActivity implements LoginListener {
     public void ok() {
         Intent i = new Intent(this, ActionsActivity.class);
         i.putExtra("username",entraNom.getText().toString());
+        i.putExtra("password",entraContra.getText().toString());
         startActivity(i);
     }
     @Override
     public void wrong() {
         Snackbar.make(entraContra, "Valores erroneos", Snackbar.LENGTH_LONG).show();
     }
+    /*public void doSomethingElse() {
+        startActivity(new Intent(MainActivity.this, ActionsActivity.class));
+        MainActivity.this.finish();
+    }*/
 }
